82b448eb70362e2b25eac61a661dcdcfe2810a49
Brian Donnoe <dunow33@hotmail.com>
2021-08-16 19:47:26 -0400

    working on getting the password condition to change text style type

--- app/src/main/java/com/example/passwordchecker/FragmentLogin.kt
+++ app/src/main/java/com/example/passwordchecker/FragmentLogin.kt
@@ -1,5 +1,6 @@
 package com.example.passwordchecker
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.passwordchecker.databinding.PasswordFieldBinding
 
-class FragmentLogin : Fragment() {
+class FragmentLogin : Fragment(R.layout.password_field) {
 
     private var _binding: PasswordFieldBinding? = null
     private var binding: PasswordFieldBinding = _binding!!
@@ -17,8 +18,18 @@ class FragmentLogin : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = PasswordFieldBinding.inflate(inflater, container, savedInstanceState)
+        _binding = PasswordFieldBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var passwordTextLength = binding.passwordBox.text.length
+        with(binding){
+            if(passwordTextLength > 3) {
+                uppercase.setTypeface(null, Typeface.BOLD)
+            }
+        }
     }
 
     override fun onDestroyView() {
