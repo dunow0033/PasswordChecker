a3c05f2260b2cfe2505b3c1091889b6f18565ada
Brian Donnoe <dunow33@hotmail.com>
2021-08-17 23:19:26 -0400

    working on password checking

--- app/src/main/java/com/example/passwordchecker/FragmentLogin.kt
+++ app/src/main/java/com/example/passwordchecker/FragmentLogin.kt
@@ -1,53 +1,39 @@
 package com.example.passwordchecker
 
-import android.graphics.Color
-import android.graphics.Typeface
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import com.example.passwordchecker.databinding.PasswordFieldBinding
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.passwordchecker.databinding.FragmentPasswordBinding
 
 class FragmentLogin : Fragment() {
 
-    private var _binding: PasswordFieldBinding? = null
-    private var binding: PasswordFieldBinding = _binding!!
+    private var _binding: FragmentPasswordBinding? = null
+    private var binding: FragmentPasswordBinding = _binding!!
 
-    var uppercase: TextView = binding.uppercase
-    var password: EditText = binding.passwordBox
+    private lateinit var viewModel: PasswordViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = PasswordFieldBinding.inflate(inflater, container, false)
+        _binding = FragmentPasswordBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(PasswordViewModel::class.java)
 
+        with(binding){
 
-        password.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-                uppercase.setTextColor(Color.BLUE)
-                uppercase.setTypeface(null, Typeface.BOLD)
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                uppercase.setTextColor(Color.BLUE)
-                uppercase.setTypeface(null, Typeface.BOLD)
-            }
-        })
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/passwordchecker/MainActivity.kt
+++ app/src/main/java/com/example/passwordchecker/MainActivity.kt
@@ -4,9 +4,4 @@ import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.password_field)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
